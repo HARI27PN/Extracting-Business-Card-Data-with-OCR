4903a7abd6f86d5c57493a9ec3456e3afbc4435d
Hariprasad Nair <hari27pn@gmail.com>
2023-05-09 00:17:13 +0530

    Update card_app.py

--- card_app.py
+++ card_app.py
@@ -129,14 +129,13 @@ if image is not None:
     with col3: 
         # DISPLAY ALL THE ELEMENTS OF BUSINESS CARD 
         st.markdown("<h2><span style='background-color: #32D7AC; color:#00008B ; font-weight: bold;'>EXTRACTED DATA</span></h2>", unsafe_allow_html=True)
-        st.markdown('##### <span style="color: white;">CARD HOLDER & COMPANY DETAILS: </span>', unsafe_allow_html=True)
-        st.markdown('##### <span style="color: white;">WEBSITE URL: </span>' + str(WEB), unsafe_allow_html=True)
-        st.markdown('##### <span style="color: white;">EMAIL ADDRESS: </span>' + str(EMAIL), unsafe_allow_html=True)
-        st.markdown('##### <span style="color: white;">PIN CODE: </span>' + str(PIN), unsafe_allow_html=True)
+        st.markdown('#### <span style="color: red;">WEBSITE URL: </span>' + str(WEB), unsafe_allow_html=True)
+        st.markdown('#### <span style="color: red;">EMAIL ADDRESS: </span>' + str(EMAIL), unsafe_allow_html=True)
+        st.markdown('#### <span style="color: red;">PIN CODE: </span>' + str(PIN), unsafe_allow_html=True)
         ph_str = ', '.join(PH)
-        st.markdown('##### <span style="color: white;">PHONE NUMBER(S): </span>' + ph_str, unsafe_allow_html=True)
+        st.markdown('#### <span style="color: red;">PHONE NUMBER(S): </span>' + ph_str, unsafe_allow_html=True)
         add_str = ' '.join([str(elem) for elem in ADD])
-        st.markdown('##### <span style="color: white;">ADDRESS: </span>' + add_str, unsafe_allow_html=True)
+        st.markdown('#### <span style="color: red;">ADDRESS: </span>' + add_str, unsafe_allow_html=True)
 
 
         IDS= [EID,PID,WID]
@@ -152,7 +151,7 @@ if image is not None:
                         numbers = re.findall('\d+', string)
                         if len(numbers) == 0 or all(len(num) < 3 for num in numbers) and not any(num in string for num in ['0','1','2','3','4','5','6','7','8','9']*3):
                             fin.append(string)
-        st.markdown("<h4 style='color:white;'>CARD HOLDER & COMPANY DETAILS:</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='color:red;'>CARD HOLDER & COMPANY DETAILS:</h4>", unsafe_allow_html=True)
         for i in fin:
             st.write('##### '+i)
             
