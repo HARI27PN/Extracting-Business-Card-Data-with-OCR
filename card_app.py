ec05f187c86fd16afd412d47593ad83c4738d5bf
Hariprasad Nair <hari27pn@gmail.com>
2023-05-09 11:08:55 +0530

    Update card_app.py

--- card_app.py
+++ card_app.py
@@ -13,7 +13,7 @@ def set_background():
         """
         <style>
         .stApp {
-            background-color: #00ff99;
+            background-color: #00ffcc;
             color: black;
         }
         </style>
