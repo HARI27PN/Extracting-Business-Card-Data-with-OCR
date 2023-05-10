680233260c5a0ca51131118780284ca2163c3af7
Hariprasad Nair <hari27pn@gmail.com>
2023-05-10 08:17:30 +0530

    Update card_app.py

--- card_app.py
+++ card_app.py
@@ -125,36 +125,35 @@ if image is not None:
         if re.match(r"(?!.*@)(www|.*com$)", string):
             WEB=string.lower()
             WID=i 
-    with col3: 
-        # DISPLAY ALL THE ELEMENTS OF BUSINESS CARD 
-        st.markdown("<h2><span style='background-color: #00008B; color:#ffffff ; font-weight: bold;'>EXTRACTED DATA</span></h2>", unsafe_allow_html=True)
-        
-        IDS= [EID,PID,WID]
-        IDS.extend(AID)
-        IDS.extend(PHID)
-#         st.write(IDS)
-        oth=''                               
-        fin=[]                        
-        for i, string in enumerate(result_text):
-            if i not in IDS:
-                if len(string) >= 4 and ',' not in string and '.' not in string and 'www.' not in string:
-                    if not re.match("^[0-9]{0,3}$", string) and not re.match("^[^a-zA-Z0-9]+$", string):
-                        numbers = re.findall('\d+', string)
-                        if len(numbers) == 0 or all(len(num) < 3 for num in numbers) and not any(num in string for num in ['0','1','2','3','4','5','6','7','8','9']*3):
-                            fin.append(string)
-        st.markdown("<h4 style='color:red;'>CARD HOLDER & COMPANY DETAILS:</h4>", unsafe_allow_html=True)
-        # Collect all the strings in fin list and join them using comma separator
-        i_values = ', '.join(fin)
+    with col3:
+        with st.container():
+            # DISPLAY ALL THE ELEMENTS OF BUSINESS CARD 
+            st.markdown("<h2><span style='background-color: #00008B; color:#ffffff ; font-weight: bold;'>EXTRACTED DATA</span></h2>", unsafe_allow_html=True)
 
-        # Write the i_values to the Streamlit app
-        st.write('#### ' + i_values)
+            IDS= [EID,PID,WID]
+            IDS.extend(AID)
+            IDS.extend(PHID)
+            # st.write(IDS)
+            oth=''                               
+            fin=[]                        
+            for i, string in enumerate(result_text):
+                if i not in IDS:
+                    if len(string) >= 4 and ',' not in string and '.' not in string and 'www.' not in string:
+                        if not re.match("^[0-9]{0,3}$", string) and not re.match("^[^a-zA-Z0-9]+$", string):
+                            numbers = re.findall('\d+', string)
+                            if len(numbers) == 0 or all(len(num) < 3 for num in numbers) and not any(num in string for num in ['0','1','2','3','4','5','6','7','8','9']*3):
+                                fin.append(string)
+            st.markdown("<h4 style='color:red;'>CARD HOLDER & COMPANY DETAILS:</h4>", unsafe_allow_html=True)
+            # Collect all the strings in fin list and join them using comma separator
+            i_values = ', '.join(fin)
 
-        st.markdown('#### <span style="color: red;">EMAIL ADDRESS: </span>' + str(EMAIL), unsafe_allow_html=True)
-        ph_str = ', '.join(PH)
-        st.markdown('#### <span style="color: red;">PHONE NUMBER(s): </span>' + ph_str, unsafe_allow_html=True)
-        st.markdown('#### <span style="color: red;">WEBSITE URL: </span>' + str(WEB), unsafe_allow_html=True)
-        add_str = ' '.join([str(elem) for elem in ADD])
-        st.markdown('#### <span style="color: red;">ADDRESS: </span>' + add_str, unsafe_allow_html=True)
-        st.markdown('#### <span style="color: red;">PIN CODE: </span>' + str(PIN), unsafe_allow_html=True)
+            # Write the i_values to the Streamlit app
+            st.write('#### ' + i_values)
 
-        
+            st.markdown('#### <span style="color: red;">EMAIL ADDRESS: </span>' + str(EMAIL), unsafe_allow_html=True)
+            ph_str = ', '.join(PH)
+            st.markdown('#### <span style="color: red;">PHONE NUMBER(s): </span>' + ph_str, unsafe_allow_html=True)
+            st.markdown('#### <span style="color: red;">WEBSITE URL: </span>' + str(WEB), unsafe_allow_html=True)
+            add_str = ' '.join([str(elem) for elem in ADD])
+            st.markdown('#### <span style="color: red;">ADDRESS: </span>' + add_str, unsafe_allow_html=True)
+            st.markdown('#### <span style="color: red;">PIN CODE: </span>' + str(PIN), unsafe_allow_html=True)
